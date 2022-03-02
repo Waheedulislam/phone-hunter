73735a0a3cb658bf35b60a38e3983cb6f0e8400d
Whaeedil <baoppyhossen1234@gmail.com>
2022-03-02 22:13:44 +0600

    fifth commit

--- js/phone.js
+++ js/phone.js
@@ -17,7 +17,7 @@ const DisplayPhoneResult = (phones) => {
     // console.log(phones);
     const searchResult = document.getElementById('search-result');
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -26,10 +26,43 @@ const DisplayPhoneResult = (phones) => {
                     <div class="card-body">
                         <h5 class="card-title">${phone.phone_name}</h5>
                         <p class="card-text">${phone.brand}</p>
-                        <button class="btn btn-primary w-50">More Detaisl</button>
+                        <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary w-50">More Explore</button>
                     </div>
                 </div>
         `;
         searchResult.appendChild(div);
     })
+}
+
+const loadPhoneDetails = phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data));
+}
+
+const displayPhoneDetail = phone => {
+    console.log(phone);
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h4 class="card-title ">${phone.name}</h4> 
+        <p class="card-text">chipSet: ${phone.mainFeatures.chipSet}</p>
+        <p class="card-text">ReleaseDate: ${phone.releaseDate}</p>
+        <p class="card-text">Brand: ${phone.brand}</p>
+        <p class="card-text">DisplaySize: ${phone.mainFeatures.displaySize}</p>
+        <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
+        <h4 class="text-center text-color">Others</h4>
+        <p class="card-text">Bluetooth: ${phone.others.Bluetooth}</p>
+        <p class="card-text">GPS: ${phone.others.GPS}</p>
+        <p class="card-text">NFC: ${phone.others.NFC}</p>
+        <p class="card-text">Radio: ${phone.others.Radio}</p>
+        <p class="card-text">USB: ${phone.others.USB}</p>
+        <p class="card-text">WLAN: ${phone.others.WLAN}</p>
+    </div>
+    `;
+    phoneDetails.appendChild(div);
 }
